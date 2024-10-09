c3d563f395a0661ab8af1569b480ce4da6b9b3b9
Simon Emms <simon@simonemms.com>
2024-10-09 09:51:38 +0000

    feat: add duration type to hcloud pkg

---
+++ pkg/hcloud/types.go
@@ -0,0 +1,36 @@
+package hcloud
+
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+type Duration struct {
+	time.Duration `json:"-"`
+}
+
+func (d Duration) MarshalJSON() ([]byte, error) {
+	return json.Marshal(d.String())
+}
+
+func (d *Duration) UnmarshalJSON(b []byte) error {
+	var v interface{}
+	if err := json.Unmarshal(b, &v); err != nil {
+		return err
+	}
+	switch value := v.(type) {
+	case float64:
+		d.Duration = time.Duration(value)
+		return nil
+	case string:
+		var err error
+		d.Duration, err = time.ParseDuration(value)
+		if err != nil {
+			return err
+		}
+		return nil
+	default:
+		return fmt.Errorf("invalid duration")
+	}
+}
